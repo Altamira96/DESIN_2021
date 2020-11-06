a95812143e7926ba3cbb975be9a686eb86bd0e88
Carlos <Carlos@PC03>
2020-11-06 19:01:11 +0100

    Escrito Hola mundo

--- primeraAplicación/src/primeraaplicación/SegundaClase.java
+++ primeraAplicación/src/primeraaplicación/SegundaClase.java
@@ -9,14 +9,6 @@ package primeraaplicación;
  *
  * @author Carlos
  */
-public class PrimeraAplicación {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
-    }
+public class SegundaClase {
     
 }
